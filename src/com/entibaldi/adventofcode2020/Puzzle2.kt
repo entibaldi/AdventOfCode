7a9dae5098c7258962cb57df4b2b22520bfe47c6
Enrico Tibaldi <etibaldi88@gmail.com>
2020-12-02 09:05:23 +0200

    puzzle 2

---
+++ src/com/entibaldi/adventofcode2020/Puzzle2.kt
@@ -0,0 +1,32 @@
+package com.entibaldi.adventofcode2020
+
+import java.io.File
+
+private val lineRegex = Regex("^(\\d+)-(\\d+) (.): (.+)\$")
+
+private data class PwdRecord(
+        val minCount: Int,
+        val maxCount: Int,
+        val c: Char,
+        val password: String
+)
+
+fun main() {
+    val input = File("res/2/input.txt").readLines().map {
+        val result: List<String> = lineRegex.matchEntire(it.trim())!!.groupValues
+        PwdRecord(result[1].toInt(), result[2].toInt(), result[3].toCharArray()[0], result[4])
+    }
+    val countValidPart1 = input.count {
+        val c = it.c
+        val min = it.minCount
+        val max = it.maxCount
+        val pwdMatchRegex = Regex("^([^$c]*$c){$min,$max}[^$c]*\$")
+        pwdMatchRegex.matches(it.password)
+    }
+    println("Valid passwords part1: $countValidPart1")
+    val countValidPart2 = input.count {
+        val chars = it.password.toCharArray()
+        sequenceOf(chars[it.minCount - 1], chars[it.maxCount - 1]).count { c -> c == it.c } == 1
+    }
+    println("Valid passwords part2: $countValidPart2")
+}
