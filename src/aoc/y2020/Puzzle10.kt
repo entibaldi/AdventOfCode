190e2f88260d4a0c4712524ce5373e30c66e7f93
Enrico Tibaldi <etibaldi88@gmail.com>
2020-12-10 17:26:38 +0200

    puzzle 10 part 2

--- src/aoc/y2020/Puzzle10.kt
+++ src/aoc/y2020/Puzzle10.kt
@@ -8,11 +8,17 @@ fun main() {
 
 class Puzzle10 : Puzzle(2020, 10) {
     override fun run() {
-        val adapters = inputLines
+        val jolts = listOf(0) + inputLines
                 .map { it.toInt() }
                 .sorted()
-        val jolts = listOf(0) + adapters + (adapters.max()!! + 3)
         val diffs = jolts.windowed(2, 1) { it[1] - it[0] }
-        println(diffs.count { it == 3 } * diffs.count { it == 1 })
+        println((diffs.count { it == 3 } + 1) * diffs.count { it == 1 })
+        val perms = longArrayOf(1, 0, 0, 0)
+        diffs.forEach { diff ->
+            perms.copyInto(perms, diff, 0, 4 - diff)
+            perms.fill(0, 0, diff)
+            perms[0] += perms.sum()
+        }
+        println(perms[0])
     }
 }
