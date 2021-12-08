da0042672f6826a628915b5f16f4ab13bf9a1879
Enrico Tibaldi <enrico.tibaldi@suunto.com>
2021-12-08 10:58:35 +0200

    O(n) fix for day 7 part 2

--- src/aoc/y2021/Puzzle7.kt
+++ src/aoc/y2021/Puzzle7.kt
@@ -2,6 +2,7 @@ package aoc.y2021
 
 import aoc.Puzzle
 import kotlin.math.abs
+import kotlin.math.roundToInt
 
 fun main() {
     Puzzle7().runAndProfile()
@@ -18,7 +19,9 @@ class Puzzle7 : Puzzle(2021, 7) {
     private fun findOptimalDistance(input: List<Int>, incrementalSteps: Boolean): Pair<Int, Int> {
         var minSum = Int.MAX_VALUE
         var minValue = Int.MAX_VALUE
-        for (v in input.minOrNull()!!..input.maxOrNull()!!) {
+        val avg = input.average().roundToInt()
+        val valuesToTest = if (incrementalSteps) avg - 1..avg + 1 else input.minOrNull()!!..input.maxOrNull()!!
+        for (v in valuesToTest) {
             val s = input.sumOf { value -> abs(value - v).let { if (incrementalSteps) it * (it + 1) / 2 else it } }
             if (minSum >= s) {
                 minSum = s
