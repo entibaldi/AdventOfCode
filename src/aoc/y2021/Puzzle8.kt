69f35ed18a2f8cce09ec2ad3b8863210af281d6d
Enrico Tibaldi <enrico.tibaldi@suunto.com>
2021-12-08 11:26:25 +0200

    cleaned up day 8

--- src/aoc/y2021/Puzzle8.kt
+++ src/aoc/y2021/Puzzle8.kt
@@ -30,8 +30,8 @@ class Puzzle8 : Puzzle(2021, 8) {
         }
     }
 
-    val digits = mapOf(0 to "abcefg", 1 to "cf", 2 to "acdeg", 3 to "acdfg", 4 to "bcdf", 5 to "abdfg", 6 to "abdefg",
-        7 to "acf", 8 to "abcdefg", 9 to "abcdfg").mapValues { (_, v) -> v.toCharArray().toSet() }
+    private val digits = mapOf(0 to "abcefg", 1 to "cf", 2 to "acdeg", 3 to "acdfg", 4 to "bcdf", 5 to "abdfg",
+        6 to "abdefg", 7 to "acf", 8 to "abcdefg", 9 to "abcdfg").mapValues { (_, v) -> v.toCharArray().toSet() }
 
     private val possibleDigitsByCount: Map<Int, Set<Int>> = mapOf(
         2 to setOf(1), 3 to setOf(7), 4 to setOf(4), 7 to setOf(8),
@@ -43,8 +43,8 @@ class Puzzle8 : Puzzle(2021, 8) {
             line.signalPatterns.forEach { computeIfAbsent(it) { mutableSetOf() }.addAll(possibleDigitsByCount[it.size]!!) }
         }
         while (pairMaps.values.any { it.size > 1 }) {
-            pairMaps.filterValues { it.size > 1 }.forEach { (chars, possibleValues) ->
-                pairMaps.filterValues { it.size == 1 }.forEach { (otherChars, valueSingle) ->
+            pairMaps.entries.stream().filter { it.value.size > 1 }.forEach { (chars, possibleValues) ->
+                pairMaps.entries.stream().filter { it.value.size == 1 }.forEach { (otherChars, valueSingle) ->
                     possibleValues.removeIf { intValue ->
                         val commonDigits = digits[intValue]!!.intersect(digits[valueSingle.single()]!!).size
                         commonDigits != chars.intersect(otherChars).size
