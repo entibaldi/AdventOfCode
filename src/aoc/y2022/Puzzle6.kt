987f9e4044e44b2d74e54ee8590b65cac930ffc2
Enrico Tibaldi <etibaldi88@gmail.com>
2022-12-06 07:58:24 +0200

    2022 day 6

---
+++ src/aoc/y2022/Puzzle6.kt
@@ -0,0 +1,24 @@
+package aoc.y2022
+
+import aoc.Puzzle
+
+fun main() {
+    Puzzle6().runAndProfile()
+}
+
+class Puzzle6 : Puzzle(2022, 6) {
+
+    override fun run() {
+        val input = inputText.toCharArray().toList()
+        println(computePart1(input))
+        println(computePart2(input))
+    }
+
+    private fun computePart1(input: List<Char>): Int = findPacket(input, 4)
+
+    private fun computePart2(input: List<Char>): Int = findPacket(input, 14)
+
+    private fun findPacket(input: List<Char>, windowSize: Int): Int =
+        input.toList().windowed(size = windowSize, step = 1)
+            .indexOfFirst { chars -> chars.distinct().size == windowSize } + windowSize
+}
